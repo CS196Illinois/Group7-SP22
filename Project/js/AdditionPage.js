dbe2fd61b91b1cbaa6dbf36b5f1daf99d13a981a
SEANO1109 <satie2@illinois.edu>
2022-03-25 21:21:02 -0500

    added functionality to home buttons

--- Project/js/AdditionPage.js
+++ Project/js/AdditionPage.js
@@ -5,11 +5,3 @@ function homeBtnClick() {
 var homeBtn = document.getElementById("home");
 
 homeBtn.addEventListener("click", homeBtnClick);
-
-document.onClick = clickListener;
-var allTimeClicks = 0;
-var currentClicks = 0;
-function clickListener(e) {
-  currentClicks += 1;
-  return currentClicks;
-}
