8901f729ed7ff72e85869ce72286d1bc0532ba3b
Julius White V <awsmguytoo@gmail.com>
2022-04-04 19:15:19 -0500

    added variable for js blocker

--- Project/js/blocker.js
+++ Project/js/blocker.js
@@ -1,9 +1,13 @@
 
-var url = 
+var inputurl;
 
+function storeurl() {
+inputurl = document.URL;
+}
 
-if (window.location.href.includes(url)) {
-    document.head.innerHTML = "WORK > $(url)"
-    document.body.innerHTML = "GET BACK TO WORK!"
-    
+document.onclick = storeurl; 
+
+if (window.location.href.includes(inputurl)) {
+    document.head.innerHTML = (`WORK > ${inputurl}`);
+    document.body.innerHTML = "GET BACK TO WORK!";
 }
