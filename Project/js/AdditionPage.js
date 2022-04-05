5ba23a82fe91831172d4c7562a156173945ac5c8
CalvinYS <csato2@illinois.edu>
2022-04-04 22:46:07 -0500

    added chrome storage

--- Project/js/AdditionPage.js
+++ Project/js/AdditionPage.js
@@ -2,15 +2,38 @@ function homeBtnClick() {
   window.location.href = "../html/home.html";
 }
 
-function submitBtnClick() {
-  if (name != "" && link != "" && time != 0) {
-    names.push(name);
-    links.push(link);
-    times.push(time);
-    console.log(names);
-    console.log(links);
-    console.log(times);
-    window.location.href = "../html/home.html";
+var value1;
+var value2;
+var value3;
+var webpages;
+
+function submitBtnClick(callback) {
+
+  document.getElementById("add-time").onclick = function() {
+
+    value1 = document.getElementById("title-input").value;
+    chrome.storage.sync.set({'title': value1}, function() {
+      console.log("Title is " + value1)
+    });
+
+    value3 = document.getElementById("time-input").value;
+    chrome.storage.sync.set({'time': value3}, function() {
+      console.log("Time is " + value3)
+    });
+
+    value2 = document.getElementById("link-input").value;
+    chrome.storage.sync.set({'websites': value2}, function() {
+      console.log("Link is " + value2)
+    });
+
+  /*  chrome.storage.sync.get(["websites"], function(items) {
+        webpages = webpages.concat(items.websites);
+        if(typeof callback === "function") {
+          callback(webpages);
+        }
+    });
+    console.log(webpages); */
+    alert("saved");
   }
 }
 
